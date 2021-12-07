6fa246b9b20f255e28adbbb3eb7db267a02afd28
MG177 <mg.terok77@gmail.com>
2021-12-07 09:25:29 +0800

    added animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import Users from "./components/user";
 import "./styles.css";
-
+import Fade from 'react-reveal/Fade';
 export default function App() {
   return (
     <div>
-    <h1>USER LIST</h1>
-      <Users/>
+      <Fade top>
+        <h1>USER LIST</h1>
+      </Fade>
+      <Fade>
+        <Users/>
+      </Fade>
     </div>
   )
 }
